134922292bb0f2239b13a28c1a291bdb2c398ec9
shreeinfinityinfotech <Admin885522@@@>
2023-03-14 16:02:15 +0530

    chirag add

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(Myapp());
@@ -9,50 +11,189 @@ class Myapp extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      title: 'hello chirag',
       home: ColumWidget(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
-class ColumWidget extends StatelessWidget {
 
+class ColumWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: nyAppbar(),
-      body: Container(
-         color: Colors.blue,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                color: Colors.red,
-                height: 100,
-                width: 100,
-              ),
-              Text("hello chirag "),
-              Container(
-                color: Colors.yellow,
-                height: 100,
-                width: 100,
-              ),
-            ],
-          )),
+      body: ContainerDemo(),
     );
   }
 }
+
 class nyAppbar extends StatelessWidget with PreferredSizeWidget {
- @override
+  @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: Text("hello chirag"),
+      title: Text("Container Demo"),
     );
   }
 
- @override
- Size get preferredSize => Size.fromHeight(kToolbarHeight);
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
 }
 
+class ContainerDemo extends StatelessWidget {
+  const ContainerDemo({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      // this widget is text Create Widget and first of row widget..
+      SizedBox(
+    height: 10,
+      ),
+      RowOfText(),
+      RowOfBoxWidget(),
+      SizedBox(
+    height: 10,
+      ),
+      RowOfimageWidget()
+    ]);
+  }
+}
+
+class RowOfText extends StatelessWidget {
+  const RowOfText({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [Text("A"), Text("B"), Text("C")],
+    );
+  }
+}
+
+class RowOfBoxWidget extends StatelessWidget {
+  const RowOfBoxWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Expanded(
+          child: Container(
+            width: 100,
+            height: 100,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: Text("Red",textAlign: TextAlign.center,),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 15),
+                  child: ElevatedButton(
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context) => leadDialog);
+                      },
+                      child: Text("click"),
+                      style: ButtonStyle(
+                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  side: BorderSide(color: Colors.yellow))))),
+                )
+              ],
+            ),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10), color: Colors.red),
+          ),
+        ),
+        Container(
+          width: 100,
+          height: 100,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Text("Green",textAlign: TextAlign.center,),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 15),
+                child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text("click"),
+                    style: ButtonStyle(
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                side: BorderSide(color: Colors.red))))),
+              )
+            ],
+          ),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10), color: Colors.green),
+        ),
+        Container(
+          width: 100,
+          height: 100,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Text(
+                  "Grey",
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 15),
+                child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text("click"),
+                    style: ButtonStyle(
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                side: BorderSide(color: Colors.white))))),
+              )
+            ],
+          ),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10), color: Colors.grey),
+        ),
+      ],
+    );
+  }
+}
+
+class RowOfimageWidget extends StatelessWidget {
+  const RowOfimageWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Container(
+            width: 300, height: 100, child: Image.asset('assets/images/b2.jpg'))
+      ],
+    );
+  }
+}
+
+Dialog leadDialog = Dialog(
+  child: Container(
+    height: 20.0,
+    width: 20.0,
+    color: Colors.white,
+    child: Text('hey', textAlign: TextAlign.center,),
+
+  ),
+);
